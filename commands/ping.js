a57c8158c5fa3be0b675b50f4ed813817f836218
Damon <enigmaoneofficial@gmail.com>
2020-02-14 19:57:22 -0500

    debugging

--- commands/ping.js
+++ commands/ping.js
@@ -2,7 +2,7 @@ module.exports = {
     config: {
         name: 'ping',
         description: 'Test command to ping back',
-        aliases: ['pig'],
+        aliases: [],
         availability: ['text', 'dm'],
         auth_level: 0,
         permitted: []
@@ -11,7 +11,6 @@ module.exports = {
         let now = Date.now()
         await client.globals.sleep(client.globals.internal.ping_timeout)
         let after = Date.now()
-        message.channel.send(`\`\`returned\`\` \`\`${(after - now) + (command.called_at - command.message_read)} ms\`\`\n\`\`after\`\` \`\`${client.globals.internal.ping_timeout} ms delayer\`\``)
+        message.channel.send(`\`\`returned\`\` \`\`${(after - now) + (command.called_at - command.message_read)} ms\`\`\n\`\`after\`\` \`\`${client.globals.internal.ping_timeout} ms delay\`\``)
     }
 }
-  
