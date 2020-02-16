90535f19a3c2868ef85deb3c53aaa76772c318d6
Damon <enigmaoneofficial@gmail.com>
2020-02-16 14:13:32 -0500

    debugging

--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -5,14 +5,10 @@ module.exports = {
   exec: async (client, reaction, user) => {
     user = await client.database.users.findOne({discord_id: user.id})
     if (reaction.emoji.name == '❌' && user.auth_level >= 9) {
-<<<<<<< HEAD
-      reaction.message.channel.send('\`\`deleting message...\`\`').then(async (message) => {
-=======
-      reaction.message.channel.send('\`\`deleting message...\`\`').then(message => {
->>>>>>> a338fd331ba029b0f08851f02d6b74e3b1420327
-        await client.globals.sleep(2000)
-        message.delete()
-      })
+      // reaction.message.channel.send('\`\`deleting message...\`\`').then(async (message) => {
+      //   await client.globals.sleep(2000)
+      //   message.delete()
+      // })
       await client.globals.sleep(2000)
       reaction.message.delete()
     }
