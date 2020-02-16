cfa2f1a364871b5595ff0dff56ad7c13f6590cba
Damon <enigmaoneofficial@gmail.com>
2020-02-16 13:51:07 -0500

    debugging

--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -4,6 +4,6 @@ module.exports = {
   },
   exec: async (client, reaction, user) => {
     user = await client.database.users.findOne({discord_id: user.id})
-    console.log(reaction)
+    console.log(reaction.emoji)
   }
 }
