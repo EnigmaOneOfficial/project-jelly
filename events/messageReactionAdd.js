1de7e4b3058a424847cebe5322d52cb0f1a018d0
Damon <enigmaoneofficial@gmail.com>
2020-02-18 11:54:04 -0500

    Updating variables for reaction actions

--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -2,28 +2,28 @@ module.exports = {
   config: {
     name: 'messageReactionAdd',
     internal: {
-      sleep_timer: 5000
+      time_to_delete: 2000
     }
   },
   exec: async (client, reaction, user, event) => {
     user = await client.database.users.findOne({discord_id: user.id})
     if (reaction.emoji.name == '❌' && user.auth_level >= 9 && reaction.message.deletable == true && reaction.message.channel.type == 'text') {
       reaction.message.channel.send('\`\`deleting\`\`').then(async (message) => {
-         await client.globals.sleep(event.internal.sleep_timer)
+         await client.globals.sleep(event.internal.time_to_delete)
          if (message.deleted == false) {
            message.delete()
          }
       })
-      await client.globals.sleep(event.internal.sleep_timer)
+      await client.globals.sleep(event.internal.time_to_delete)
       reaction.message.delete()
     } else if (reaction.emoji.name == '💣' && user.auth_level >= 9 && reaction.message.deletable == true && reaction.message.channel.type == 'text') {
       reaction.message.channel.send(`\`\`purging ${reaction.message.author.username}\'s messages\`\``).then(async (message) => {
-         await client.globals.sleep(event.internal.sleep_timer)
+         await client.globals.sleep(event.internal.time_to_delete)
          if (message.deleted == false) {
            message.delete()
          }
       })
-      await client.globals.sleep(event.internal.sleep_timer)
+      await client.globals.sleep(event.internal.time_to_delete)
       reaction.message.channel.fetchMessages({limit: 100}).then(messages => {
         messages = messages.filter(message => (message.author.id == reaction.message.author.id && message.deletable == true))
         reaction.message.channel.bulkDelete(messages)
