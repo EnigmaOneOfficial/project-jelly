328c53831245c2cc8b94095323aed1f58d8ae58e
Damon <enigmaoneofficial@gmail.com>
2020-02-18 19:09:07 -0500

    Removing purge all messages from user

--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -14,20 +14,6 @@ module.exports = {
            message.delete()
          }
       })
-      await client.globals.sleep(event.internal.time_to_delete)
-      reaction.message.delete()
-    } else if (reaction.emoji.name == '💣' && user.auth_level >= 9 && reaction.message.deletable == true && reaction.message.channel.type == 'text') {
-      reaction.message.channel.send(`\`\`purging ${reaction.message.author.username}\'s messages\`\``).then(async (message) => {
-         await client.globals.sleep(event.internal.time_to_delete)
-         if (message.deleted == false) {
-           message.delete()
-         }
-      })
-      await client.globals.sleep(event.internal.time_to_delete)
-      await reaction.message.channel.fetchMessages({limit: 100, before: reaction.message.id}).then(messages => {
-        messages = messages.filter(message => (message.author.id == reaction.message.author.id && message.deletable == true))
-        reaction.message.channel.bulkDelete(messages)
-      })
     }
   }
 }
