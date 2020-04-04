47c0ea02df40b075ae10ef85741d8cf91129aa58
Damon Gillombardo <enigmaoneofficial@gmail.com>
2020-04-03 21:33:18 -0400

    verify.js fix

--- commands/verify.js
+++ commands/verify.js
@@ -10,29 +10,29 @@ module.exports = {
     exec: async (client, message, command) => {
         let email = command.message.content.split(' ')[1]
         let random = Math.floor(Math.random() * 90000) + 10000
-        await client.globals.nodemailer.sendMail({
+        let info = await client.globals.nodemailer.sendMail({
           from: 'ohioesports.noreply@gmail.com',
           to: email,
           subject: 'Discord Verification',
           html: `To finish verification, reply to the bot with the code <b>${random}</b>`
-        }, async (err, info) => {
-          if (err) {
-            message.channel.send(`\`\`Could not send email to ${email}\`\``)
-          } else {
-            client.database.users.findOneAndUpdate({discord_id: message.author.id}, {
-              $set: {
-                verification: {
-                  status: 'verifying',
-                  code: random,
-                  email: email,
-                  domain: email.slice(email.search(/(?<=@)(.+)/), email.length),
-                  guild: message.guild.id
-                }
-              }
-            })
-            await message.delete({timeout: 1000})
-            await message.author.send(`\`\`An email has been sent to ${email}\`\`\n\`\`Please enter the 5 digit code that was sent\`\``)
-          }
         })
+        if (!info) {
+          message = await message.channel.send(`\`\`Could not send email to ${email}\`\``)
+          await message.delete({timeout: 2000})
+        } else {
+          client.database.users.findOneAndUpdate({discord_id: message.author.id}, {
+            $set: {
+              verification: {
+                status: 'verifying',
+                code: random,
+                email: email,
+                domain: email.slice(email.search(/(?<=@)(.+)/), email.length),
+                guild: message.guild.id
+              }
+            }
+          })
+          await message.delete({timeout: 1000})
+          await message.author.send(`\`\`An email has been sent to ${email}\`\`\n\`\`Please enter the 5 digit code that was sent\`\``)
+        }
     }
 }
